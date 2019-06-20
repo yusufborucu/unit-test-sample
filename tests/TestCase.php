1889a5f5136751cf00fe6d34bcae34cb43b8c80a
Yusuf Borucu <yusufborucu@gmail.com>
2019-06-20 12:18:51 +0300

    first commit

---
+++ tests/TestCase.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests;
+
+use Faker\Factory;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+    use CreatesApplication, DatabaseMigrations;
+
+    protected $faker;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->faker = Factory::create();
+    }
+}
